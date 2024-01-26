ac36eae8e2105395bfcc027094a92a2e80555ed9
大排面加蛋 <798989631@qq.com>
2024-01-26 23:35:51 +0800

    2024-01-26 扩展REPL

--- repl/repl.go
+++ repl/repl.go
@@ -21,6 +21,7 @@ const PROMPT = ">>"
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
 	env := object.NewEnvironment()
+	macroEnv := object.NewEnvironment()
 
 	for {
 		fmt.Fprintf(out, PROMPT)
@@ -39,8 +40,11 @@ func Start(in io.Reader, out io.Writer) {
 			printParserErrors(out, p.Errors())
 			continue
 		}
+		evaluator.DefineMacros(program, macroEnv)
+		expanded := evaluator.ExpandMacros(program, macroEnv)
 
-		evaluated := evaluator.Eval(program, env)
+		// evaluated := evaluator.Eval(program, env)
+		evaluated := evaluator.Eval(expanded, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
@@ -51,6 +55,7 @@ func Start(in io.Reader, out io.Writer) {
 		//for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
 		//	fmt.Fprintf(out, "%s\n", tok.Literal)
 		//}
+
 	}
 }
 
